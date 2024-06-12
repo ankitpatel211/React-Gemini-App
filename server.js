cd00f0b18f2b7d514905b7f645c0a02249727856
Ankit Patel <wd.ankit.patel@gmail.com>
2024-06-12 21:36:54 +0530

    React web google gemini ai chatbot

---
+++ server.js
@@ -0,0 +1,21 @@
+const PORT = 8000
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+const { GoogleGenerativeAI } = require('@google/generative-ai')
+const genAI = new GoogleGenerativeAI('AIzaSyB2D5DbHbnrxoBrnchaBFRRlcftCM0RG_E')
+app.post('/gemini', async (req, res) => {
+    const model = genAI.getGenerativeModel({model:"gemini-pro"})
+    const history = req.body.history
+    const chat = model.startChat(history)
+    const message = req.body.message
+    const result = await chat.sendMessage(message)
+    const response = result.response
+    const text = response.text()
+    console.log(text)
+    res.send(text)
+})
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState("")
+  const [error, setError] = useState("")
+  const [chatHistory, setChatHistory] = useState([])
+
+  const surpriseOptions = [
+    "Who won the latest novel peace prize?",
+    "Where does the pizza come from?",
+    "Who do you make a BLT sandwich?"
+  ]
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
+  const getResponse = async () => {
+    if (!value) {
+      setError("Error: Please ask a question!")
+      return
+    }
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: value,
+          history: chatHistory
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+      console.log(JSON.stringify(options.body))
+      const response = await fetch('http://localhost:8000/gemini', options)
+      const data = await response.text()
+      setChatHistory(oldChatHistory => [...oldChatHistory, 
+        {
+          role: "User",
+          parts: value
+        }, 
+        {
+          role: "Gemini",
+          parts: data 
+        }
+      ])
+      console.log(JSON.stringify(chatHistory))
+      setValue("")
+    } catch (error) {
+      console.error(error)
+      setError("Something went wrong! Please try again later.") 
+    }
+  }
+
+  const clear = () => {
+    setValue("")
+    setError("")
+    setChatHistory([])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>
+        What do you want to know?
+        <button className="surprise" onClick={surprise} disabled={!chatHistory}>
+          Surprise Me
+        </button>
+        <button className="surprise" onClick={clear}>
+          Clear
+        </button>
+      </p>
+      <div className="input-container">
+        <input
+          value={value}
+          placeholder="When is Christmas...?"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {!error && <button onClick={getResponse}>Ask me</button>}
+        {error && <button onClick={clear}>Clear</button>}
+      </div>
+      {error && <p>{error}</p>}
+      <div className="search-result">
+        {
+          chatHistory.map(
+            (chatItem, _index) => 
+              <div key={_index}>
+                <p className="answer"><b>{chatItem.role}:</b> {chatItem.parts}</p>
+              </div>
+          )
+        }
+      </div>
     </div>
   );
 }
